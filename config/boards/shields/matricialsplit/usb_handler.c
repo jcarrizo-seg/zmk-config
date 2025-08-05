75942e5091f009e6bb358d58a1af2a101f22c78e
Carrizo, Joel <jcarrizo@adminseg.com>
2025-08-05 00:13:53 -0300

    Cambios simples.

--- config/boards/shields/matricialsplit/usb_handler.c
+++ config/boards/shields/matricialsplit/usb_handler.c
@@ -1,11 +1,8 @@
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/event_manager.h>
-#include <zmk/endpoints.h>
-#include <zmk/hid.h>
 
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+// Sin logs por ahora - usaremos indicadores visuales
 
 // Work queue para enviar teclas de manera asíncrona
 static struct k_work_delayable usb_connected_work;
@@ -14,22 +11,19 @@ static void send_usb_indicator(struct k_work *work) {
     // Esperar a que USB esté completamente establecido
     k_sleep(K_MSEC(2000));
     
-    // Por ahora solo dejamos el log para confirmar que funciona
-    // TODO: Implementar envío de teclas cuando tengamos la API correcta
-    LOG_INF("¡USB conectado! Aquí enviaríamos las teclas USB");
+    // Por ahora, solo una pausa para confirmar que se ejecuta
+    // TODO: Implementar envío de teclas o LEDs
 }
 
 static int usb_connection_listener(const zmk_event_t *eh) {
     struct zmk_usb_conn_state_changed *ev = as_zmk_usb_conn_state_changed(eh);
     
     if (ev->conn_state == ZMK_USB_CONN_HID) {
-        LOG_INF("USB conectado - enviando indicador");
-        // Programar el envío de teclas
+        // USB conectado - programar indicador
         k_work_schedule(&usb_connected_work, K_MSEC(100));
         
     } else if (ev->conn_state == ZMK_USB_CONN_NONE) {
-        LOG_INF("USB desconectado - funcionando con batería");
-        // Cancelar trabajo pendiente si existe
+        // USB desconectado - cancelar trabajo pendiente
         k_work_cancel_delayable(&usb_connected_work);
     }
     
